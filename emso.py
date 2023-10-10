2095a69190391ff6aaff63ea203c82d2e362093e
truongvck3313 <116653217+truongvck3313@users.noreply.github.com>
2023-10-10 16:24:39 +0700

    Add files via upload

---
+++ emso.py
@@ -0,0 +1,28 @@
+import funcion
+
+
+
+
+def dang_nhap(self):
+    funcion.login.khong_thanh_cong_tk_emso1(self)
+    funcion.login.khong_thanh_cong_tk_emso2(self)
+    funcion.login.thanh_cong_tk_emso(self)
+
+    funcion.login.chon_tk_dang_nhap_gan_day_khong_nhap_pass(self)
+    funcion.login.chon_tk_dang_nhap_gan_day_nhap_sai_pass(self)
+    funcion.login.chon_tk_dang_nhap_gan_day_chua_luu_mk(self)
+    funcion.login.chon_tk_dang_nhap_gan_day_da_luu_mk(self)
+
+    funcion.login.thanh_cong_tk_google1(self)
+    # funcion.login.thanh_cong_tk_google2(self) #để chạy cuối, đang lỗi
+
+
+
+def trangcanhan(self):
+    # funcion.thongtincanhan_anhdaidien(self)
+    # funcion.thongtincanhan_anhbia(self)
+    # funcion.gioithieu.gioithieu_tongquan(self)
+    # funcion.gioithieu.congviec_vahocvan(self)
+    # funcion.gioithieu.xemsukientrongdoi_congviecvahocvan(self)
+    # funcion.gioithieu.trangcanhan_gioithieu_congviecvahocvan_addthem(self)
+    funcion.gioithieu.trangcanhan_gioithieu_thongtincoban(self)
